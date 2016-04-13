6bb052e7ba7b15652cf1e225d3b3a9de0d5f70f0
Fabrice Servais <fabrice.servais@gmail.com>
2016-04-13 09:59:05 +0200

    Functions to handle IPs : to_binaary_string, distance between IPs, longest prefix from String

--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-from utils import plot_to_file, plot_pie_to_file
+from utils import *
 import ipaddress
 
-CHUNKSIZE = 10 ** 6
+CHUNKSIZE = 10 ** 5
 
 # dfs = pd.read_csv("netflow_100000.csv", chunksize=CHUNKSIZE, iterator=True)
 #
@@ -20,9 +20,11 @@ durations = np.array([])
 port_traffic_sender = pd.DataFrame({'sp': [-1], 'ibyt': [-1], 'obyt': [-1]}, index=[0]) # Init with row of port -1 to set the columns as integers
 port_traffic_receiver = pd.DataFrame({'dp': [-1], 'ibyt': [-1], 'obyt': [-1]}, index=[0])
 
+traffic_by_ip = pd.DataFrame({'sa': [-1], 'ibyt': [-1], 'obyt': [-1]}, index=[0])
+
 i = 0
 df_save = pd.DataFrame()
-for df in pd.read_csv("netflow.csv", chunksize=CHUNKSIZE, iterator=True):
+for df in pd.read_csv("netflow_2000000.csv", chunksize=CHUNKSIZE, iterator=True):
     i += 1
     print("Chunk number {}".format(i))
     df = df.dropna()
@@ -42,105 +44,134 @@ for df in pd.read_csv("netflow.csv", chunksize=CHUNKSIZE, iterator=True):
     gb_receiver = port_traffic_receiver.groupby('dp')
     port_traffic_receiver = gb_receiver.sum().reset_index()
 
+    # Compute traffic by IP
+    traffic_by_ip = pd.concat([traffic_by_ip, df[['sa', 'ibyt', 'obyt']]])
+    gb_ip = traffic_by_ip.groupby('sa')
+    traffic_by_ip = gb_ip.sum().reset_index()
+
+
+
 # -- Post-processing
 
-port_traffic_sender = port_traffic_sender[port_traffic_sender.sp != -1]
-port_traffic_sender['bytes_tot'] = port_traffic_sender['ibyt'] + port_traffic_sender['obyt']
+# port_traffic_sender = port_traffic_sender[port_traffic_sender.sp != -1]
+# port_traffic_sender['bytes_tot'] = port_traffic_sender['ibyt'] + port_traffic_sender['obyt']
+#
+# port_traffic_receiver = port_traffic_receiver[port_traffic_receiver.dp != -1]
+# port_traffic_receiver['bytes_tot'] = port_traffic_receiver['ibyt'] + port_traffic_receiver['obyt']
+#
 
-port_traffic_receiver = port_traffic_receiver[port_traffic_receiver.dp != -1]
-port_traffic_receiver['bytes_tot'] = port_traffic_receiver['ibyt'] + port_traffic_receiver['obyt']
+# IP
+traffic_by_ip = traffic_by_ip[traffic_by_ip['sa'] != -1]
+traffic_by_ip['sa'] = traffic_by_ip['sa'].apply(lambda x: ipaddress.ip_address(x))
+traffic_by_ipv4 = traffic_by_ip[traffic_by_ip['sa'].apply(lambda x: isinstance(x, ipaddress.IPv4Address))]
+traffic_by_ipv6 = traffic_by_ip[traffic_by_ip['sa'].apply(lambda x: isinstance(x, ipaddress.IPv6Address))]
 
-# ---- COMPUTATIONS
+traffic_by_ipv4_sorted = traffic_by_ipv4.sort_values(by=['sa'])
+traffic_by_ipv6_sorted = traffic_by_ipv6.sort_values(by=['sa'])
 
-# ---- Average packet size
+for i in range(0, len(traffic_by_ipv4_sorted)):
+    begin = traffic_by_ipv4_sorted[i]
 
-ipkt_size = ibyts/ipkts
-ipkt_size = ipkt_size[~np.isnan(ipkt_size)]
-if ipkt_size.size > 0:
-    print("Average packet size (input): {0:.2f}\n".format(ipkt_size.mean()))
+    j = 1
+    end = traffic_by_ipv4_sorted[i+1]
+    while dist_ips(begin, end) < 254:
+        j += 1
+        end = traffic_by_ipv4_sorted[i + j]
 
-opkt_size = obyts/opkts
-opkt_size = opkt_size[~np.isnan(opkt_size)]
-if opkt_size.size > 0:
-    print("Average packet size (output): {0:.2f}\n".format(opkt_size.mean()))
+    end = traffic_by_ipv4_sorted[i+j-1]
 
-# ---- CCDF
-# -- Compute
 
-# Flow duration
-durations_values, durations_base = np.histogram(durations, bins=40)
-durations_values = durations_values / len(durations)
-durations_cumulative = np.cumsum(durations_values)
-
-# Number of bytes (in)
-ibyts_values, ibyts_base = np.histogram(ibyts, bins=40)
-ibyts_values = ibyts_values / len(ibyts)
-ibyts_cumulative = np.cumsum(ibyts_values)
-
-# Number of bytes (out)
-obyts_values, obyts_base = np.histogram(obyts, bins=40)
-obyts_values = obyts_values / len(obyts)
-obyts_cumulative = np.cumsum(obyts_values)
-
-# Number of packets (in)
-ipkts_values, ipkts_base = np.histogram(ipkts, bins=40)
-ipkts_values = ipkts_values / len(ipkts)
-ipkts_cumulative = np.cumsum(ipkts_values)
-
-# Number of packets (out)
-opkts_values, opkts_base = np.histogram(opkts, bins=40)
-opkts_values = opkts_values / len(opkts)
-opkts_cumulative = np.cumsum(opkts_values)
-
-# -- Draw
-
-# Flow duration
-plot_to_file(file_name="ccdf_durations", title="CCDF of flow duration", x=durations_base[:-1], y=1-durations_cumulative, xlabel="Duration", ylabel="Complementary Cumulative Probability Distribution")
-plot_to_file(file_name="ccdf_durations_log", title="CCDF of flow duration", x=durations_base[:-1], y=1-durations_cumulative, xlabel="Duration", ylabel="Complementary Cumulative Probability Distribution", scale='log')
-
-# Number of bytes (in)
-plot_to_file(file_name="ccdf_ibyts", title="CCDF of number of bytes (in)", x=ibyts_base[:-1], y=1-ibyts_cumulative, xlabel="Number of bytes (in)", ylabel="Complementary Cumulative Probability Distribution")
-plot_to_file(file_name="ccdf_ibyts_log", title="CCDF of number of bytes (in)", x=ibyts_base[:-1], y=1-ibyts_cumulative, xlabel="Number of bytes (in)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
-
-# Number of bytes (out)
-plot_to_file(file_name="ccdf_obyts", title="CCDF of number of bytes (out)", x=obyts_base[:-1], y=1-obyts_cumulative, xlabel="Number of bytes (out)", ylabel="Complementary Cumulative Probability Distribution")
-plot_to_file(file_name="ccdf_obyts_log", title="CCDF of number of bytes (out)", x=obyts_base[:-1], y=1-obyts_cumulative, xlabel="Number of bytes (out)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
-
-# Number of packets (in)
-plot_to_file(file_name="ccdf_ipkts", title="CCDF of number of packets (in)", x=ipkts_base[:-1], y=1-ipkts_cumulative, xlabel="Number of packets (in)", ylabel="Complementary Cumulative Probability Distribution")
-plot_to_file(file_name="ccdf_ipkts_log", title="CCDF of number of packets (in)", x=ipkts_base[:-1], y=1-ipkts_cumulative, xlabel="Number of packets (in)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
-
-# Number of packets (out)
-plot_to_file(file_name="ccdf_opkts", title="CCDF of number of packets (out)", x=opkts_base[:-1], y=1-opkts_cumulative, xlabel="Number of packets (out)", ylabel="Complementary Cumulative Probability Distribution")
-plot_to_file(file_name="ccdf_opkts_log", title="CCDF of number of packets (out)", x=opkts_base[:-1], y=1-opkts_cumulative, xlabel="Number of packets (out)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
-
-# ---- Port traffic
-sum_bytes_sender = port_traffic_sender['bytes_tot'].sum()
-sum_bytes_receiver = port_traffic_receiver['bytes_tot'].sum()
-
-port_traffic_sender['bytes_tot_per'] = port_traffic_sender['bytes_tot'] / sum_bytes_sender
-port_traffic_receiver['bytes_tot_per'] = port_traffic_receiver['bytes_tot'] / sum_bytes_receiver
-
-top_10_sender = port_traffic_sender.sort_values(by=['bytes_tot_per'], ascending=[False])[0:10]
-top_10_receiver = port_traffic_receiver.sort_values(by=['bytes_tot_per'], ascending=[False])[0:10]
-not_top_10_sender = port_traffic_sender.sort_values(by=['bytes_tot_per'], ascending=[False])[10:]
-not_top_10_receiver = port_traffic_receiver.sort_values(by=['bytes_tot_per'], ascending=[False])[10:]
-
-# -- Sender
-port_sender_list = top_10_sender['sp'].values.tolist()
-port_sender_list.append("Others")
-labels_sender = tuple(port_sender_list)
-values_sender = top_10_sender['bytes_tot_per'].values.tolist()
-values_sender.append(not_top_10_sender['bytes_tot_per'].sum())
-
-plot_pie_to_file("top_10_ports_sender", values_sender, labels_sender, "Top 10 ports (sender)")
-
-# -- Receiver
-port_receiver_list = top_10_receiver['dp'].values.tolist()
-port_receiver_list.append("Others")
-labels_receiver = tuple(port_receiver_list)
-values_receiver = top_10_receiver['bytes_tot_per'].values.tolist()
-values_receiver.append(not_top_10_receiver['bytes_tot_per'].sum())
-
-plot_pie_to_file("top_10_ports_receiver", values_receiver, labels_receiver, "Top 10 ports (receiver)")
+# # ---- COMPUTATIONS
+#
+# # ---- Average packet size
+#
+# ipkt_size = ibyts/ipkts
+# ipkt_size = ipkt_size[~np.isnan(ipkt_size)]
+# if ipkt_size.size > 0:
+#     print("Average packet size (input): {0:.2f}\n".format(ipkt_size.mean()))
+#
+# opkt_size = obyts/opkts
+# opkt_size = opkt_size[~np.isnan(opkt_size)]
+# if opkt_size.size > 0:
+#     print("Average packet size (output): {0:.2f}\n".format(opkt_size.mean()))
+#
+# # ---- CCDF
+# # -- Compute
+#
+# # Flow duration
+# durations_values, durations_base = np.histogram(durations, bins=40)
+# durations_values = durations_values / len(durations)
+# durations_cumulative = np.cumsum(durations_values)
+#
+# # Number of bytes (in)
+# ibyts_values, ibyts_base = np.histogram(ibyts, bins=40)
+# ibyts_values = ibyts_values / len(ibyts)
+# ibyts_cumulative = np.cumsum(ibyts_values)
+#
+# # Number of bytes (out)
+# obyts_values, obyts_base = np.histogram(obyts, bins=40)
+# obyts_values = obyts_values / len(obyts)
+# obyts_cumulative = np.cumsum(obyts_values)
+#
+# # Number of packets (in)
+# ipkts_values, ipkts_base = np.histogram(ipkts, bins=40)
+# ipkts_values = ipkts_values / len(ipkts)
+# ipkts_cumulative = np.cumsum(ipkts_values)
+#
+# # Number of packets (out)
+# opkts_values, opkts_base = np.histogram(opkts, bins=40)
+# opkts_values = opkts_values / len(opkts)
+# opkts_cumulative = np.cumsum(opkts_values)
+#
+# # -- Draw
+#
+# # Flow duration
+# plot_to_file(file_name="ccdf_durations", title="CCDF of flow duration", x=durations_base[:-1], y=1-durations_cumulative, xlabel="Duration", ylabel="Complementary Cumulative Probability Distribution")
+# plot_to_file(file_name="ccdf_durations_log", title="CCDF of flow duration", x=durations_base[:-1], y=1-durations_cumulative, xlabel="Duration", ylabel="Complementary Cumulative Probability Distribution", scale='log')
+#
+# # Number of bytes (in)
+# plot_to_file(file_name="ccdf_ibyts", title="CCDF of number of bytes (in)", x=ibyts_base[:-1], y=1-ibyts_cumulative, xlabel="Number of bytes (in)", ylabel="Complementary Cumulative Probability Distribution")
+# plot_to_file(file_name="ccdf_ibyts_log", title="CCDF of number of bytes (in)", x=ibyts_base[:-1], y=1-ibyts_cumulative, xlabel="Number of bytes (in)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
+#
+# # Number of bytes (out)
+# plot_to_file(file_name="ccdf_obyts", title="CCDF of number of bytes (out)", x=obyts_base[:-1], y=1-obyts_cumulative, xlabel="Number of bytes (out)", ylabel="Complementary Cumulative Probability Distribution")
+# plot_to_file(file_name="ccdf_obyts_log", title="CCDF of number of bytes (out)", x=obyts_base[:-1], y=1-obyts_cumulative, xlabel="Number of bytes (out)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
+#
+# # Number of packets (in)
+# plot_to_file(file_name="ccdf_ipkts", title="CCDF of number of packets (in)", x=ipkts_base[:-1], y=1-ipkts_cumulative, xlabel="Number of packets (in)", ylabel="Complementary Cumulative Probability Distribution")
+# plot_to_file(file_name="ccdf_ipkts_log", title="CCDF of number of packets (in)", x=ipkts_base[:-1], y=1-ipkts_cumulative, xlabel="Number of packets (in)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
+#
+# # Number of packets (out)
+# plot_to_file(file_name="ccdf_opkts", title="CCDF of number of packets (out)", x=opkts_base[:-1], y=1-opkts_cumulative, xlabel="Number of packets (out)", ylabel="Complementary Cumulative Probability Distribution")
+# plot_to_file(file_name="ccdf_opkts_log", title="CCDF of number of packets (out)", x=opkts_base[:-1], y=1-opkts_cumulative, xlabel="Number of packets (out)", ylabel="Complementary Cumulative Probability Distribution", scale='log')
+#
+# # ---- Port traffic
+# sum_bytes_sender = port_traffic_sender['bytes_tot'].sum()
+# sum_bytes_receiver = port_traffic_receiver['bytes_tot'].sum()
+#
+# port_traffic_sender['bytes_tot_per'] = port_traffic_sender['bytes_tot'] / sum_bytes_sender
+# port_traffic_receiver['bytes_tot_per'] = port_traffic_receiver['bytes_tot'] / sum_bytes_receiver
+#
+# top_10_sender = port_traffic_sender.sort_values(by=['bytes_tot_per'], ascending=[False])[0:10]
+# top_10_receiver = port_traffic_receiver.sort_values(by=['bytes_tot_per'], ascending=[False])[0:10]
+# not_top_10_sender = port_traffic_sender.sort_values(by=['bytes_tot_per'], ascending=[False])[10:]
+# not_top_10_receiver = port_traffic_receiver.sort_values(by=['bytes_tot_per'], ascending=[False])[10:]
+#
+# # -- Sender
+# port_sender_list = top_10_sender['sp'].values.tolist()
+# port_sender_list.append("Others")
+# labels_sender = tuple(port_sender_list)
+# values_sender = top_10_sender['bytes_tot_per'].values.tolist()
+# values_sender.append(not_top_10_sender['bytes_tot_per'].sum())
+#
+# plot_pie_to_file("top_10_ports_sender", values_sender, labels_sender, "Top 10 ports (sender)")
+#
+# # -- Receiver
+# port_receiver_list = top_10_receiver['dp'].values.tolist()
+# port_receiver_list.append("Others")
+# labels_receiver = tuple(port_receiver_list)
+# values_receiver = top_10_receiver['bytes_tot_per'].values.tolist()
+# values_receiver.append(not_top_10_receiver['bytes_tot_per'].sum())
+#
+# plot_pie_to_file("top_10_ports_receiver", values_receiver, labels_receiver, "Top 10 ports (receiver)")
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from ipaddress import *
+from os.path import commonprefix
 
 import numpy as np
 
@@ -28,3 +30,31 @@ def plot_pie_to_file(file_name, values, labels, title):
     plt.savefig("{}.pdf".format(file_name))
 
     plt.close()
+
+def dist_ips(ip1, ip2):
+    dist = 0;
+    for subnet in summarize_address_range(ip1, ip2):
+        ips_in_subnet = list(subnet.subnets(new_prefix=32))
+        dist += len(ips_in_subnet)
+
+    return dist
+
+def binary_str_ipv4(ip):
+    bytes = v4_int_to_packed(int(ip))
+    binary = 0;
+    for byte in bytes:
+        binary = (binary << 8) | byte
+
+    binary_without_front_0 = bin(binary)[2:]
+    return '0' * (32-len(binary_without_front_0)) + binary_without_front_0
+
+    return bin(binary)[2:]
+    # return bin((((((bytes[0] << 8) | (bytes[1])) << 8) | bytes[2]) << 8) | bytes[3])[2:] #
+
+def length_longest_prefix(ips):
+    ips = [binary_str_ipv4(ip) for ip in ips]
+    lengths = [len(ip) for ip in ips]
+    print(lengths)
+    prefix = commonprefix(ips)
+    print(prefix)
+    return len(prefix)
